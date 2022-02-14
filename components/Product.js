7b5dcab799668dd3445c1bfd0edb9e180df5631c
KuroYami08 <74539525+KuroYami08@users.noreply.github.com>
2022-02-14 17:52:13 +0800

    card

--- components/Product.js
+++ components/Product.js
@@ -1,4 +1,5 @@
 import {
+  Grid,
   Card,
   CardActionArea,
   CardMedia,
@@ -9,6 +10,7 @@ import React, { useState } from "react";
 import { postToJSON } from "../lib/firebase";
 import "firebase/compat/firestore";
 import NextLink from "next/link";
+import data from "../utils/data";
 
 //
 
@@ -24,47 +26,57 @@ export async function getServerSideProps() {
   //   console.log(post);
   //   console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-  return {
-    props: { posts }, // will be passed to the page component as props
-  };
+  // return {
+  //   props: { posts }, // will be passed to the page component as props
+  // };
 }
 
 export default function ProductCard(props) {
-  const [posts, setPosts] = useState(props.posts);
+  // const [posts, setPosts] = useState(props.posts);
 
-  const missingProduct = posts.filter((missingItems) => {
-    return missingProduct;
-  });
+  // const missingProduct = posts.filter((missingItems) => {
+  //   return missingProduct;
+  // });
 
   return (
     <div>
-      {missingProduct.map((card) => (
-        <Card>
-          <CardMedia>
-            component="img" title={card.itemLost}
-            image={card.itemLost}
-            height={300}
-            width={300}
-            alt={"no image"}
-          </CardMedia>
-          <CardActionArea>
-            <Typography>Item Lost: {card.itemLost}</Typography>
-          </CardActionArea>
-          <CardActionArea>
-            <Typography>Location: {card.location}</Typography>
-          </CardActionArea>
-          <NextLink href={"/"} passHref>
-            <CardActionArea>
-              <Link>View Lost Item</Link>
-            </CardActionArea>
-          </NextLink>
-          <NextLink href={"/"} passHref>
-            <CardActionArea>
-              <Link>Found It?</Link>
-            </CardActionArea>
-          </NextLink>
-        </Card>
-      ))}
+      <Grid container spacing={3}>
+        {data.products.map((products) => (
+          <Grid item md={4} key={products.brand}>
+            <Card>
+              <CardMedia
+                component="img"
+                title={products.category}
+                image={products.image}
+                height={300}
+                width={300}
+                alt={"no image"}
+              ></CardMedia>
+              <CardActionArea>
+                <Typography>Item Lost: {products.itemLost}</Typography>
+              </CardActionArea>
+              <CardActionArea>
+                <Typography>Email: {products.email}</Typography>
+              </CardActionArea>
+              <CardActionArea>
+                <Typography>
+                  Location: {products.placeLocation} - {products.mapLocation}
+                </Typography>
+              </CardActionArea>
+              <NextLink href={"/"} passHref>
+                <CardActionArea>
+                  <Link>View Lost Item</Link>
+                </CardActionArea>
+              </NextLink>
+              <NextLink href={"/"} passHref>
+                <CardActionArea>
+                  <Link>Found It?</Link>
+                </CardActionArea>
+              </NextLink>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
