da5776e10a386dfc589b789017713d942da30b44
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2022-02-18 21:23:40 +0800

    adding firebase userContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import { useEffect } from "react";
-
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import { useUserData } from "../lib/hooks";
+import { UserContext } from "../lib/context";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
@@ -12,9 +15,11 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <UserContext.Provider value={userData}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </UserContext.Provider>
   );
 }
 export default MyApp;
