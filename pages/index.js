954e4a588c59ad8462738d2fe5bf8462968bc1b5
KuroYami08 <74539525+KuroYami08@users.noreply.github.com>
2022-03-07 17:07:57 +0800

    added guidelines

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,94 @@ export default function Home() {
         <Grid item xs={6}>
           <Image src={announcement} />
         </Grid>
-
+        <Grid item xs={12}>
+          <Typography variant="h3">
+            <b>Community Finds Guidelines</b>
+          </Typography>
+          <br />
+          <Typography variant="h5" style={{ textAlign: "justify" }}>
+            When you use Community Finds, you join a community of people from
+            Barangay Balibago who can post their lost or found item within the
+            area. Aside from that, persons who are not residents of the region
+            but are certain to have lost their item/s inside the area may
+            utilize the Community Finds by uploading the specifics of their lost
+            item/s and other necessary information. This is to provide the
+            residents and nonresidents a greater opportunity of finding their
+            lost items. Community Finds is also a place to provide inspiration
+            and motivation to others to post their found item and to return it
+            to the rightful owner. The guidelines below help keep the community
+            to fully understand and make it possible to understand Community
+            Finds.
+          </Typography>
+        </Grid>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={12}>
+          <Typography variant="h3">
+            <b>Important Guidelines</b>
+          </Typography>
+          <br />
+          <Typography variant="h5">
+            <li> Treat others online as you would treat them in real life. </li>
+            <li>
+              Respect the personal information of other community members.
+            </li>
+            <li>
+              Communicate with courtesy and respect through the contact
+              information provided.
+            </li>
+            <li>Complete all the details needed in the lost and found form.</li>
+            <li>
+              Use proper judgment or discretion while interacting with other
+              users;
+            </li>
+            <li>
+              Community Finds recommend you to go to barangay hall or police
+              station to do the claiming or handing items for security purposes.
+            </li>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={12}></Grid>
+        <Grid item xs={5}>
+          <Typography variant="h4">
+            <b>Please do not:</b>
+          </Typography>
+          <br />
+          <Typography variant="h6">
+            <li>
+              Do not post content that is not related to a lost or found item.
+            </li>
+            <li>
+              Do not post content that is disrespectful, malicious or offensive.
+            </li>
+            <li> Do not spam any contact information given by other users.</li>
+            <li>
+              Do not trick other members of the community, who are simply
+              seeking for their lost items.
+            </li>
+          </Typography>
+        </Grid>
+        <Grid item xs={7}>
+          <Typography variant="h4">
+            <b>Security and Safety:</b>
+          </Typography>
+          <br />
+          <Typography variant="h6">
+            <li>
+              Community Finds will provide an email verification, to ensure the
+              email address is existent, active and valid.
+            </li>
+            <li>
+              Community Finds will provide an email authentication, to prove
+              that the email is not forged and it belongs to a user who claims
+              to be from.
+            </li>
+            <li>
+              Community Finds make sure the collection of information that you
+              will provide are secured following the data Privacy law.
+            </li>
+          </Typography>
+        </Grid>
         <Grid item xs={4} style={{ marginTop: "60px" }}>
           <center>
             <Image src={ease} height={200} width={300} />
@@ -57,7 +144,6 @@ export default function Home() {
             required information and let our system go to work.
           </Typography>
         </Grid>
-
         <Grid item xs={4} style={{ marginTop: "60px" }}>
           <center>
             <Image src={match} height={200} width={300} />
