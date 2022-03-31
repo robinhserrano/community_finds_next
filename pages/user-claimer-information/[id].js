bc3104cc47d66e411a53e971bdf7858c7f4b6839
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2022-03-31 19:36:04 +0800

    edited useEffect

--- pages/user-claimer-information/[id].js
+++ pages/user-claimer-information/[id].js
@@ -23,21 +23,6 @@ export default function UserClaimerInformation(props) {
   const [posts, setPosts] = useState(props.posts);
   const router = useRouter();
   const { id } = router.query;
-  //NEWCODE
-  const [currentUser, setUser] = useState();
-
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem("user");
-    if (loggedInUser) {
-      setUser(loggedInUser);
-      const profileName = posts.filter((doc) => {
-        return doc.id.includes(loggedInUser);
-      });
-    } else {
-      router.push("/login");
-    }
-  }, []);
-  //NEWCODE
 
   const slugClient = posts.filter((items) => {
     return items.id.toLowerCase().includes(id);
